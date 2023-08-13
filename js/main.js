353ce486954c1e3d1d760ffffeb3f9a5d49b5b91
KIM M S <61860897+kju1018@users.noreply.github.com>
2023-08-13 23:48:03 +0900

    Design: 공지사항 추가
    - 공지사항 추가
    - 슬라이드 추가를 위해 Swiperjs 라이브러리 추가

--- js/main.js
+++ js/main.js
@@ -46,3 +46,9 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1,
   })
 });
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
+});
